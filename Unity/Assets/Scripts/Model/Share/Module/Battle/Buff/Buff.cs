6d7c33480406aaf72d539a9fbeacacbe14529a5e
stone <>
2025-03-15 01:39:53 +0800

    添加Buff Model

---
+++ Unity/Assets/Scripts/Model/Share/Module/Battle/Buff/Buff.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace ET
+{
+    [ChildOf(typeof(BuffComponent))]
+    public class Buff : Entity, IAwake<int>, IDestroy
+    {
+        [BsonIgnore]
+        public Unit Unit => this.GetParent<Unit>();
+
+        public int BuffId;
+        public long StartTime;
+        public long IntervalTime;
+        public uint LayerCount;
+
+        [BsonIgnore]
+        public TbSkillBuffRow TbBuffRow => this.tbBuffRow ??= TbSkillBuff.Instance.Get(BuffId);
+        [BsonIgnore]
+        private TbSkillBuffRow tbBuffRow;
+    }
+}
---
+++ Unity/Assets/Scripts/Model/Share/Module/Battle/Buff/BuffComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Options;
+
+namespace ET
+{
+    [ComponentOf(typeof(Unit))]
+    public class BuffComponent : Entity, IAwake, IDestroy
+    {
+        [BsonDictionaryOptions(DictionaryRepresentation.ArrayOfArrays)]
+        public SortedDictionary<int, EntityRef<Buff>> IdBuffMapping = new();
+    }
+}
