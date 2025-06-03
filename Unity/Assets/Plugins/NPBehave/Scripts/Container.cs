eaed22f2e5bf0fd4ca7dc2313d174023718ab01b
alsostone <alsostone@163.com>
2025-06-03 20:22:08 +0800

    NPBehave 使用MemoryPack序列化

--- Unity/Assets/Plugins/NPBehave/Scripts/Container.cs
+++ Unity/Assets/Plugins/NPBehave/Scripts/Container.cs
@@ -1,5 +1,4 @@
 using MemoryPack;
-using UnityEngine.Assertions;
 
 namespace NPBehave
 {
@@ -26,8 +25,6 @@ namespace NPBehave
 
         public void ChildStopped(Node child, bool succeeded)
         {
-            // Assert.AreNotEqual(this.currentState, State.INACTIVE, "The Child " + child.Name + " of Container " + this.Name + " was stopped while the container was inactive. PATH: " + GetPath());
-            Assert.AreNotEqual(this.currentState, State.INACTIVE, "A Child of a Container was stopped while the container was inactive.");
             this.DoChildStopped(child, succeeded);
         }
 
